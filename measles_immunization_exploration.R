3a3dacd0dd893499d466921d66b4e06b7dde53cc
Pallavi Rane <97990797+palsrane@users.noreply.github.com>
2024-05-21 09:35:09 -0700

    Added new plots
    Added new plots for MMR and Overall vaccination rates by the type of school.

--- measles_immunization_exploration.R
+++ measles_immunization_exploration.R
@@ -6,7 +6,7 @@ library(pivottabler)
 
 `%notin%` <- Negate(`%in%`)
 
-setwd("C:/Users/palla/Desktop/GoogleDataAnalyticsCapstoneProject")
+setwd("C:/Users/palla/Desktop/GoogleDataAnalyticsCapstoneProject/VaccinationRatesInUSSchools")
 
 #Cleaning data----
 
@@ -79,32 +79,36 @@ write_csv(select(wsj1,-"new_id"),"measles_nonduplicated.csv")
 # Looking at the data on Tableau revealed another problem with the data. 86 of the records, 
 # 3 from Florida and 83 from Vermont had incorrect longitudes, and this problem persisted 
 # in the source files where the latitudes and longitudes were taken from. 
+clean_states[clean_states$lng>0,]
 # Upon checking with google maps, it was clear that in some cases the longitudes were marked
 # as +ve where should have been -ve. 
-
+tmp=wsj1[wsj1$lng>0,]
+tmp[!is.na(tmp$name),]
+count(tmp[!is.na(tmp$name),])
+rm(tmp)
 # However, in most coases, it looked like the lattitudes were incorrectly entered as longitudes.
 # Since it was not possible to correct each and every case manually, it was decided that 
 # for these 86 cases, the incorrect longitudes would be replaced with generated longitudes 
 # usually assigned to the respective state by Tableau.
 # Tableau assigns -72.7678 to Vermont and -81.55 to Florida.
+wsj1$lng[(wsj1$state=="Vermont" & wsj1$lng>0)]= -72.7678
+#One record from Florida that had the correct value but marked +ve
+wsj1$lng[(wsj1$state=="Florida" & wsj1$lng>80)]= -1* wsj1$lng[(wsj1$state=="Florida" & wsj1$lng>0)] 
+wsj1$lng[(wsj1$state=="Florida" & wsj1$lng>0)]= -81.55
 
+write_csv(select(wsj1,-"new_id"),"measles_nonduplicated_ModifiedIncorrectLng.csv")
 #----
 
 # Data review ----
-vacc_rec=read_csv("measles_nonduplicated.csv")
+vacc_rec=read_csv("measles_nonduplicated_ModifiedIncorrectLng.csv")
 
 ### Checking for any NAs in mmr and overall column
 length(vacc_rec$mmr[is.na(vacc_rec$mmr)])
 length(vacc_rec$overall[is.na(vacc_rec$overall)])
-ggplot(data = vacc_rec) +
-  geom_point(mapping = aes(x = overall, y = mmr, color=state)) +
-  labs(x=('Overall vaccination rate (%)'),y='MMR vaccination rate (%)')
 
 ### The lack of vaccination report is marked as -1 in the dataset.
 ### Unfortunately almost 44% or 20,176 of the schools in this report did not report overall vaccination rates,and 
 ### 39% or 18,087of the schools did not report any mmr vaccination rates
-#### These are noted as -1 in the table
-
 ### Percentage of schools that did not report overall vaccination rates
 paste(as.character(round(count(vacc_rec[vacc_rec$overall==-1,])/count(vacc_rec)*100,2)),"%")
 length(vacc_rec$overall[vacc_rec$overall==-1])
@@ -113,20 +117,9 @@ length(vacc_rec$overall[vacc_rec$overall==-1])
 paste(as.character(round(count(vacc_rec[vacc_rec$mmr==-1,])/count(vacc_rec)*100,2)),"%")
 length(vacc_rec$mmr[vacc_rec$mmr==-1])
 
-### 26,234 schools reported overall vaccination rates
-length(vacc_rec$overall[vacc_rec$overall!=-1])
-
-ggplot(data=vacc_rec[vacc_rec$overall!=-1,],aes(y=overall))+
-  geom_histogram(binwidth=1,color="blue",alpha=0.5) +
-  labs(title='Number of Schools vs % of students with overall vaccination', x='Number of schools',y='Overall Vaccination (%)')
-
-### Almost 92% of the schools that reported overall vaccinations had 80% or more of the kids vaccinated and 
-### less than 1.3% of the schools had vaccinataion rate under 50%.
-
-#Percentage of schools with >=80% overall vacciation of those that reported
-paste(as.character(round(count(vacc_rec[vacc_rec$overall>=80,])/count(vacc_rec[vacc_rec$overall!=-1,])*100,2)),"%")
-#Percentage of schools with <50% overall vacciation of those that reported
-paste(as.character(round(count(vacc_rec[vacc_rec$overall<50 & vacc_rec$overall!=-1,])/count(vacc_rec[vacc_rec$overall!=-1,])*100,2)),"%")
+ggplot(data = vacc_rec) +
+  geom_point(mapping = aes(x = overall, y = mmr, color=type)) +
+  labs(x=('Overall vaccination rate (%)'),y='MMR vaccination rate (%)') 
 
 
 
@@ -144,6 +137,66 @@ ggplot(data=vacc_rec[vacc_rec$mmr!=-1,], aes(x=mmr)) +
 paste(as.character(round(count(vacc_rec[vacc_rec$mmr>=80,])/count(vacc_rec[vacc_rec$mmr!=-1,])*100,2)),"%")
 #Percentage of schools with <50% overall vacciation of those that reported
 paste(as.character(round(count(vacc_rec[vacc_rec$mmr<50 & vacc_rec$mmr!=-1,])/count(vacc_rec[vacc_rec$mmr!=-1,])*100,2)),"%")
+
+ggplot(data = vacc_rec[vacc_rec$mmr!=-1 & !is.na(vacc_rec$type),]) +
+  geom_bar(mapping = aes(x = type, fill=type)) +
+  labs(title='MMR Vaccination rates by type of school',x=('Type of school'),
+       y='Number of schools reporting MMR') +
+  scale_fill_manual(values=c('BOCES' =  'firebrick2', 'Charter' = 'goldenrod',
+                             'Kindergarten' = 'green3','Private' = 'blue', 'Nonpublic' = 'turquoise',
+                             'Public' = 'deeppink'))+ 
+  theme(axis.text.x = element_text(angle = 90))
+
+ggplot(data = vacc_rec[vacc_rec$mmr!=-1 & !is.na(vacc_rec$type),]) +
+  geom_point(mapping = aes(x = type, y = mmr, color=type)) +
+  labs(title='MMR Vaccination rates by type of school',x=('Type of school'),
+       y='MMR vaccination rate (%)') +
+  scale_color_manual(values=c('BOCES' =  'firebrick2', 'Charter' = 'goldenrod',
+                              'Kindergarten' = 'green3','Private' = 'blue', 'Nonpublic' = 'turquoise',
+                              'Public' = 'deeppink'))+ 
+  theme(axis.text.x = element_text(angle = 90))
+
+
+
+
+
+
+### 26,234 schools reported overall vaccination rates
+length(vacc_rec$overall[vacc_rec$overall!=-1])
+
+ggplot(data=vacc_rec[vacc_rec$overall!=-1,],aes(y=overall))+
+  geom_histogram(binwidth=1,color="blue",alpha=0.5) +
+  labs(title='Number of Schools vs % of students with overall vaccination', x='Number of schools',y='Overall Vaccination (%)')
+
+### Almost 92% of the schools that reported overall vaccinations had 80% or more of the kids vaccinated and 
+### less than 1.3% of the schools had vaccinataion rate under 50%.
+
+#Percentage of schools with >=80% overall vacciation of those that reported
+paste(as.character(round(count(vacc_rec[vacc_rec$overall>=80,])/count(vacc_rec[vacc_rec$overall!=-1,])*100,2)),"%")
+#Percentage of schools with <50% overall vacciation of those that reported
+paste(as.character(round(count(vacc_rec[vacc_rec$overall<50 & vacc_rec$overall!=-1,])/count(vacc_rec[vacc_rec$overall!=-1,])*100,2)),"%")
+
+tmp = mutate(vacc_rec,overall_stat= ifelse(overall!=-1,1,-1))
+tmp = tmp[!is.na(tmp$type),]
+ggplot() +
+  geom_bar(data = vacc_rec[vacc_rec$overall!=-1 & !is.na(vacc_rec$type),],mapping = aes(x = type, fill=type)) +
+  #geom_bar(data = vacc_rec[!is.na(vacc_rec$type),],mapping = aes(x = type, fill=type,color = )) +
+  labs(title='Overall Vaccination rates by type of school',x=('Type of school'),
+       y='Number of schools reporting Overall vaccinations') +
+  scale_fill_manual(values=c('BOCES' =  'firebrick2', 'Charter' = 'goldenrod',
+                              'Kindergarten' = 'green3','Private' = 'blue', 'Nonpublic' = 'turquoise',
+                              'Public' = 'deeppink'))+ 
+  theme(axis.text.x = element_text(angle = 90))
+
+ggplot(data = vacc_rec[vacc_rec$overall !=-1 & !is.na(vacc_rec$type),]) +
+  geom_point(mapping = aes(x = type, y = overall, color=type)) +
+  labs(title='Overall Vaccination rates by type of school',x=('Type of school'),
+       y='Overall vaccination rate (%)') +
+  scale_color_manual(values=c('BOCES' =  'firebrick2', 'Charter' = 'goldenrod',
+                              'Kindergarten' = 'green3','Private' = 'blue', 'Nonpublic' = 'turquoise',
+                              'Public' = 'deeppink'))+ 
+  theme(axis.text.x = element_text(angle = 90))
+
 
 
 
